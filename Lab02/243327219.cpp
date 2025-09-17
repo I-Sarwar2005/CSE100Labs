178bd59d38aa6a60e1ad3b921ec78ab3a334be63
I-Sarwar2005 <ibraheem.s.sarwar@gmail.com>
2025-09-17 10:48:27 -0700

    initialized

---
+++ Lab02/243327219.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+    void mergeSort(vector<int>& arr, int n){
+
+    }
+
+int main(){
+
+    int n;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    mergeSort(arr,n);
+
+}
